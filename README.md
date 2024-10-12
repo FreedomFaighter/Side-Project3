78fe634e8e99fcf8fa0b3600a046b416d2676627
John H. Freeman <john@freemania.pro>
2024-10-12 12:00:40 -0400

    removed comments from main and added to README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+# Side-Project3
+Asks the user what they earned for each month of the years 2018 and 2017
--- main.java
+++ main.java
@@ -1,6 +1,3 @@
-# Side-Project3
-Asks the user what they earned for each month of the years 2018 and 2017
-
 import java.util.Scanner;
 
 public class Chapter8PPMatthewRoberts {
